7e51de2402507b7bb42fc8bb57de9e36a355dfe3
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-02-09 14:57:44 +0700

    sum paths

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,18 @@
 package;
 
 import flixel.FlxGame;
+import openfl.display.FPS;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	public static var fpsCounter:FPS;
+
 	public function new()
 	{
 		super();
 		addChild(new FlxGame(0, 0, PlayState));
+		fpsCounter = new FPS(10, 10, 0xFFFFFF);
+		addChild(fpsCounter);
 	}
 }
---
+++ source/Paths.hx
@@ -0,0 +1,19 @@
+package;
+
+class Paths
+{
+	inline public static function image(paths:String):String
+	{
+		return 'assets/images/' + paths;
+	}
+
+	inline public static function music(paths:String):String
+	{
+		return 'assets/music/' + paths;
+	}
+
+	inline public static function sounds(paths:String):String
+	{
+		return 'assets/sounds/' + paths;
+	}
+}
